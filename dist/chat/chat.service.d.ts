37916914581e8328f5055e1a644b4eeab09a3df1
omer ahmed <omer.al7labe.oa@gmail.com>
2024-11-06 12:22:33 +0200

    Delete dist directory

--- dist/chat/chat.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from 'typeorm';
-import { Message } from './message.entity';
-import { User } from '../users/user.entity';
-export declare class ChatService {
-    private messageRepository;
-    constructor(messageRepository: Repository<Message>);
-    saveMessage(content: string, sender: User): Promise<Message>;
-}
--- dist/chat/message.entity.d.ts
+++
@@ -1,7 +0,0 @@
-import { User } from '../users/user.entity';
-export declare class Message {
-    id: number;
-    sender: User;
-    content: string;
-    createdAt: Date;
-}
